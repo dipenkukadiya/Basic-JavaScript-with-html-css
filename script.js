ebf40dd4d398180aa77406b01890668cfa51249b
Dipen kukadiya <dipenkukadiya@gmail.com>
2023-10-26 19:33:56 +0530

    first ommit

---
+++ script.js
@@ -0,0 +1,30 @@
+gsap.to("#page1"),{
+    scrollTrigger:{
+        trigger:'#page1',
+        start:'top top',
+        end: 'bottom top',
+        marker:true,
+        pin:true,
+        scroller:'#main'
+    }
+}
+gsap.to("#page1"),{
+    scrollTrigger:{
+        trigger:'#page1',
+        start:'top top',
+        end: 'bottom top',
+        marker:true,
+        pin:true,
+        scroller:'#main'
+    }
+}
+gsap.to("#page1"),{
+    scrollTrigger:{
+        trigger:'#page1',
+        start:'top top',
+        end: 'bottom top',
+        marker:true,
+        pin:true,
+        scroller:'#main'
+    }
+}
